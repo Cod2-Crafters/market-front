d6718d47f456dda7f1d818f744b50261fcfdd587
salkizzang <choiwsx@gmail.com>
2024-02-18 23:13:55 +0900

    Feat: products 앱 라우터 추가

---
+++ src/features/products/ProductsClient.tsx
@@ -0,0 +1,18 @@
+
+
+
+function ProductsClient() {
+
+    return (
+        <main>
+            <div>
+                <div className="py-8">
+                    이미지 영역
+                </div>
+            </div>
+
+        </main>
+    )
+}
+
+export default ProductsClient;
