e0b58d42ee0a302d9ac3b26e398a1f9df65cada5
tars-c <securemem@icloud.com>
2024-03-03 23:33:21 +0900

    Fix: page.tsx 서버 컴포넌트로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,97 +1,116 @@
-'use client';
-
+import ToggleGroupButton from '@/components/buttons/ToggleGroupButton';
+import CategoryData from '@/components/custom/CategoryData';
+import { ProductFormNameInput } from '@/components/custom/ProductFormNameInput';
+import ProductFormPriceInput from '@/components/custom/ProductFormPriceInput';
+import ProductFormTagInput from '@/components/custom/ProductFormTagInput';
+import FileUploader from '@/components/Files/FileUploader';
+import Icon from '@/components/icons/Icon';
+import Header from '@/components/page/Header';
+import DescTextarea from '@/components/texts/DescTextarea';
+import SectionWrapper from '@/components/wrappers/SectionWrapper';
 import initMocks from '@/services/mocks';
-import { Icon } from '@iconify/react';
-import Image from 'next/image';
-import Link from 'next/link';
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
     initMocks();
     console.log('init Mocks');
 }
+export interface ICategory {
+    id: number;
+    name: string;
+    count: number;
+    parentId: number;
+    selected?: boolean;
+    child?: Array<ICategory>;
+}
 
-console.log('page init1');
+async function getCategories() {
+    // actions
+    const res = await fetch(`http://13.125.249.102:8080/api/category/list`, { cache: 'no-cache' });
+    return res.json();
+}
 
-export default function Home() {
+export default async function Home() {
     // const [reviews, setReviews] = useState<string[] | null>(null);
 
     // const handleGetReviews = () => {
     //     // Client-side request are mocked by `mocks/browser.ts`.
-    //     fetch('/Linkpi/users')
+    //     fetch('/api/users')
     //         .then((res) => res.json())
     //         .then((data) => console.log('outputdata', data));
     // };
 
+    // let categorylist: ICategory[] = await getCategories();
     return (
         <>
-            <div className="min-w-[1236px]">
-                <header className="m-auto flex w-full max-w-screen-lg flex-1 flex-col">
-                    <nav className="flex flex-1 justify-between border-b border-neutral-500">
-                        <ul className="flex space-x-5">
-                            <li>앱다운로드</li>
-                            <li>즐겨찾기</li>
-                        </ul>
-                        <ul className="flex space-x-5">
-                            <li>로그인</li>
-                            <li>회원가입</li>
-                            <li>고객센터</li>
-                        </ul>
-                    </nav>
-                    <nav className="flex flex-1 justify-between py-2">
-                        <Link href="/">
-                            <Image src="/images/logo.svg" alt="logo" width={148} height={42} />
-                        </Link>
-                        {/* searchbar - 검색 시 main content 변겨앟는 방식으로 구현 */}
-                        <div className="flex min-w-[400px] items-center border-2 border-primary px-1  ">
-                            <input className="w-full flex-1 border-2" placeholder="상품명, 지역명,@상점명 입력" />
-                            <div>
-                                <Icon className="text-secondary" icon="ic:baseline-search" width={32} height={32} />
-                            </div>
-                        </div>
-                        <div>
-                            <ul className="flex w-full items-center space-x-3">
-                                {/* 전부 main content가 변경하는 방식으로 구현하기 */}
-                                <li>
-                                    {/* 판매하기 - /products/new */}
-                                    <Link href="#">
-                                        <Icon
-                                            className="text-primary"
-                                            icon="healthicons:money-bag"
-                                            width={40}
-                                            height={40}
-                                        />
-                                    </Link>
-                                </li>
-                                <li>
-                                    {/* 찜 목록 - /shop/{shopid(userid)}/favorites */}
-                                    <Link href="#">
-                                        <Icon className="text-primary" icon="mdi:heart-outline" width={40} height={0} />
-                                    </Link>
-                                </li>
-                                <li>
-                                    {/* 번개톡 - 추후 구현 */}
-                                    <Link href="#">
-                                        <Icon className="text-primary" icon="zondicons:chat-bubble-dots" width={32} />
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                    <div className="flex items-center justify-between">
-                        <div>
-                            <Icon icon="cil:hamburger-menu" width={34} height={34} />
-                        </div>
-                        <div className="color-white rounded-full bg-primary px-4 py-1 text-white">판매하기</div>
-                    </div>
-                </header>
-                <main className="flex flex-col items-center justify-between">
+            <div className="flex min-w-[1236px] flex-col">
+                <Header />
+                <main className="flex flex-col items-center">
                     {/* 메인페이지 광고 영역  */}
-                    <div className="flex h-24 w-full bg-zinc-700">main ads</div>
-                    <div className="m-auto flex h-48 w-full max-w-screen-lg flex-col bg-orange-600">main content</div>
+                    <div className="h-24 flex w-full bg-zinc-700">main ads</div>
+                    {/* 메인 시작 */}
+                    <div className="flex h-96 w-full max-w-screen-lg flex-col">
+                        <div className="flex flex-col">
+                            {/* 카메라 */}
+                            <SectionWrapper>
+                                <>
+                                    <div className="relative w-[150px] bg-gray-60">
+                                        <div className='transform" absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center '>
+                                            <Icon icon="bi:camera" width={64} height={0} />
+                                            <span className="m-auto">0 / 10</span>
+                                        </div>
+                                        <FileUploader />
+                                    </div>
+                                </>
+                            </SectionWrapper>
+                            <SectionWrapper>
+                                <ProductFormNameInput />
+                            </SectionWrapper>
+                            <SectionWrapper>
+                                <CategoryData />
+                            </SectionWrapper>
+                            <SectionWrapper>
+                                <ProductFormPriceInput />
+                            </SectionWrapper>
+                            <SectionWrapper>
+                                <ToggleGroupButton
+                                    title="교환"
+                                    items={[
+                                        { value: '1', text: '가능' },
+                                        { value: '0', text: '불가' },
+                                    ]}
+                                />
+                            </SectionWrapper>
+                            <SectionWrapper>
+                                <DescTextarea
+                                    placeholderComp={
+                                        <p className="text-gray-30">
+                                            <span className="text-lg">
+                                                구매시기, 브랜드/모델명, 제품의 상태 (사용감, 하자 유무) 등을 입력해
+                                                주세요. 서로가 믿고 거래할 수 있도록, 자세한 정보와 다양한 각도의 상품
+                                                사진을 올려주세요.
+                                            </span>
+                                            <br />
+                                            <span className="text-sm">
+                                                * 안전하고 건전한 거래 환경을 위해 과학기술정보통신부,
+                                                한국인터넷진흥원과 태풍마켓(주)가 함께 합니다.
+                                            </span>
+                                        </p>
+                                    }
+                                />
+                            </SectionWrapper>
+                            <SectionWrapper>
+                                <ProductFormTagInput />
+                            </SectionWrapper>
+                            <br />
+                        </div>
+                    </div>
+                    {/* <FlyOut>
+                        <FlyOutToggle></FlyOutToggle>
+                    </FlyOut> */}
                 </main>
-                <footer className="m-auto flex h-24 w-full max-w-screen-lg flex-col bg-orange-300">
+                {/* <footer className="mt-30 m-auto flex h-24 w-full max-w-screen-lg flex-col bg-orange-300">
                     이 영역은 푸터입니다
-                </footer>
+                </footer> */}
             </div>
         </>
     );
