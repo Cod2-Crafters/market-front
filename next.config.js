d42b0d37bb93aa009cb5592367d00d74326c7624
tars-c <securemem@icloud.com>
2024-03-25 17:56:12 +0900

    Style: redux 모듈 설치, svgr 모듈 설치 Style: 테마 색상 변경

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    compiler: {
+        removeConsole: process.env.NODE_ENV === 'production',
+    },
+    webpack(config) {
+        // @svg load
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+        });
 
-module.exports = nextConfig
+        return config;
+    },
+};
+
+module.exports = nextConfig;
